5fe22ef8f8fe9eb0874c0441c22e14965e41ce48
vishal <vishalbhojwani917@gmail.com>
2025-06-05 18:33:17 +0530

    updated schema

--- models/Image.js
+++ models/Image.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  category: { type: String, required: true },
+category: {
+  type: String,
+  required: true,
+  enum: ['nature', 'science', 'travel', 'sports', 'technology']
+},
   tags: [String],
   isPublic: { type: Boolean, default: false },
   imageUrl: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Image", imageSchema);
+module.exports = mongoose.model("Image", imageSchema);  
