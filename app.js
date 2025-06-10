e90d2e576b8d9049948960da421dda93b4e5ae4c
vishal <vishalbhojwani917@gmail.com>
2025-06-10 15:04:06 +0530

    Portno  fixed

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ import imageRoutes from "./routes/routes.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-dotenv.config();
+const PORT = process.env.PORT ;
+console.log(process.env.PORT)
+// dotenv.config();
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "public")));
 
@@ -20,6 +21,7 @@ async function connectDB() {
   try {
     await mongoose.connect(process.env.DB_CONNECT_STRING);
     console.log("Connected to MongoDB");
+    console.log("Database URL:", process.env.DB_CONNECT_STRING);            
   } catch (err) {
     console.error("MongoDB connection failed:", err);
   }
